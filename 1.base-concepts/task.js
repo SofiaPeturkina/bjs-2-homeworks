31127a9994e55f4a1802ea9a281a945887c1333f
Sofia Peturkina <peturkina@bk.ru>
2021-09-05 13:00:16 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr;
+  } else if (d == 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  } 
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
