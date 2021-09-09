dd958751dc778aef5f7797a3b968a99a33d1fbb1
Sofia Peturkina <peturkina@bk.ru>
2021-09-09 19:56:13 +0300

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length !== arr2.length) {
+    return result = false;
+  } 
+  result = arr1.every((value, idx) => arr1[value, idx] === arr2[value, idx]);
 
   return result; // boolean
 }
@@ -9,7 +12,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((positive) => positive >= 0).filter((numerals) => numerals % 3 === 0).map((multiply) => multiply * 10);
 
   return resultArr; // array
 }
