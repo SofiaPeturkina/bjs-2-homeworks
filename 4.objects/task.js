171845c4ba313db000c4558ad9fcf6bbd2820731
Sofia Peturkina <peturkina@bk.ru>
2021-09-12 14:20:54 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,56 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+  return this.subject[subjectName];
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks) {
+    this.marks.push(mark);
+  } else {
+    this.marks = [mark];    
+  }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  this.marks = [...marks];
+}
+
+Student.prototype.getAverage = function(addMarks) {
+  let sum = 0;
+
+  for (let mark of this.marks) {
+    sum += mark;
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  this.excluded = reason;
+
+  delete this.subjectName;
+  delete this.marks;
+}
+
+let student1 = new Student("Tony", "male", 37);
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
+console.log(student1.getAverage());
+
+console.log(student1);
+
+let student2 = new Student("Buzz", "female", 35);
+student2.setSubject("Geometry");
+student2.addMark(2);
+student2.addMark(3);
+student2.addMark(2);
+student2.exclude('low grades')
+
+console.log(student2)
